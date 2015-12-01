67427b26967d524b107bde17c673085d1a27a71e
Graham Ollis <plicease@cpan.org>
2015-12-01 09:10:36 -0500

    Initial commit

---
+++ lib/Test/Clustericious/Blocking.pm
@@ -0,0 +1,9 @@
+package Test::Clustericious::Blocking;
+
+use strict;
+use warnings;
+
+# ABSTRACT: Run blocking code in a process using an unholy combination of forks and Mojolicious
+# VERSION
+
+1;
